4ca7d911cb107536380403a51122ee30eda15fa7
Nik Medvedev <MedvedevKlaus@yandex.ru>
2020-11-20 15:13:51 +0300

    add consol.log for result

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const reducer = (accumulator, currentValue) => accumulator * currentValue;
 
 arrNum = num.toString().split('');
 result = arrNum.reduce(reducer);
+console.log(result);
 result **= 3;
 
 alert(result.toString().substr(0,2));
