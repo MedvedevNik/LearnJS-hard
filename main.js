3e032ff73aab457df75907e3c379572fadca823e
Nik Medvedev <MedvedevKlaus@yandex.ru>
2020-11-23 17:09:10 +0300

    lesson_3

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-let num = 266219,
-    arrNum,
-    result;
-const reducer = (accumulator, currentValue) => accumulator * currentValue;
+let lang = prompt('Введите одно из двух значений ("ru" или "en")', 'ru');
 
+if ( lang === 'ru' ) {
+  console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else if ( lang === 'en' ) {
+  console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+} else {
+  console.log('Неверное значение');
+}
 
-arrNum = num.toString().split('');
-result = arrNum.reduce(reducer);
-console.log(result);
-result **= 3;
+// через switch
 
-alert(result.toString().substr(0,2));
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+    break;
+  case 'en':
+    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+    break;
+  default: 
+    console.log('Неверное значение');
+}
+
+//многомерный массив
+
+const langArray = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+console.log(langArray[lang]); 
+
+//Задание 2
+
+let namePerson = prompt('Введите ваше имя', 'Николай');
+
+namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
