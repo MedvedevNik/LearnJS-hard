92f4ca74f99c536ebe5e1bfc3aec59bc9ec061af
Nik Medvedev <MedvedevKlaus@yandex.ru>
2020-11-24 10:54:30 +0300

    add lesson_4 and try callback function

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
-let lang = prompt('Введите одно из двух значений ("ru" или "en")', 'ru');
+'use strict';
 
-if ( lang === 'ru' ) {
-  console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-} else if ( lang === 'en' ) {
-  console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-} else {
-  console.log('Неверное значение');
+// const str = prompt('Введите предложение', '  Hi  '),
+//       maxLength = 30;
+// let string = str.trim();
+
+// const stringCheck = function(string, callback) {
+//   if (typeof string.charAt(0) === 'number') {
+//     alert('Введенный аргумент не строка');
+//   } else {
+//     callback();
+//   }
+// }
+
+// stringCheck(string, function(){
+//   if (string.length < maxLength) {
+//     console.log(string);
+//   } else {
+//     console.log(string.substr(0, maxLength) + '...');
+//   }
+// });
+
+
+// попытка сделать через callback
+
+const maxLength = 30;
+
+const stringCheck = function(str) {
+  if (typeof str === 'string') {
+    const string = str.trim();
+    if (string.length < maxLength) {
+      return string;
+    } else {
+      return string.substr(0, maxLength) + '...'; 
+    }
+  }
+  return 'Не является строкой';
 }
 
-// через switch
-
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-    break;
-  case 'en':
-    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-    break;
-  default: 
-    console.log('Неверное значение');
-}
-
-//многомерный массив
-
-const langArray = {
-    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-};
-console.log(langArray[lang]); 
-
-//Задание 2
-
-let namePerson = prompt('Введите ваше имя', 'Николай');
-
-namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
+console.log('stringCheck(   Привет ):' + stringCheck('   Привет '));
+console.log(stringCheck('   Привет здесь ооочень много символов'));
+console.log('stringCheck(100): ', stringCheck(100));
