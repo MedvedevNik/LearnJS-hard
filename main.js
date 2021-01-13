3344a3fc91d4cc7420caf31a7f3b6f1f8221972c
Nik Medvedev <MedvedevKlaus@yandex.ru>
2021-01-13 14:50:32 +0300

    feat: create random color

--- main.js
+++ main.js
@@ -1,49 +1,18 @@
 'use strict';
-// Предыдущее исправление урока №4 (теперь можно проверить на )
-const maxLength = 30;
 
-const stringCheck = function(str) {
-  if (isNaN(+str)) {
-    const string = str.trim();
-    if (string.length < maxLength) {
-      return string;
-    } else {
-      return string.substr(0, maxLength) + '...'; 
+const getRandomColor = () => {
+    let letters = '0123456789ABCDEF',
+        color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-  }
-  return 'Не является строкой';
+    return color;
 };
 
-console.log('stringCheck(   Привет ):' + stringCheck('   Привет '));
-console.log(stringCheck('   Привет здесь ооочень много символов'));
-console.log('stringCheck(100): ', stringCheck('293476'));
+const setBodyColor = () => {
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+    document.querySelector('h1').textContent = newColor;
+};
 
-
-// задание №5 
-
-let arr = [];
-
-for (let i = 0; i < 7; i++) {
-  arr[i] = String(Math.floor(Math.random() * (1000 - 1)) + 1);
-}
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-      console.log(arr[i]);
-  }
-}
-
-console.log(arr);
-
-// ничего лучше не придумал и не нашел...
-nextPrime:
-for (let i = 2; i <= 100; i++) { 
-
-  for (let j = 2; j < i; j++) { 
-    if (i % j == 0) {
-      continue nextPrime;
-    } 
-  }
-
-  console.log(i + ': Делитель этого числа: 1 и', i) ;
-}
+document.querySelector('button').addEventListener('click', setBodyColor);
