3c158b4953b48162c1492ccb4520b930766b66a9
Nik Medvedev <MedvedevKlaus@yandex.ru>
2021-02-07 19:51:21 +0300

    feat: create new function

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 'use strict';
 
-const getRandomColor = () => {
-    let letters = '0123456789ABCDEF',
-        color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+const timeDisplay = () => {
+    const week = ['Воскресенье, ', 'Понедельник, ', 'Вторник, ', 'Среда, ',
+        'Четверг, ', 'Пятница, ', 'Суббота, '];
+    const month = [' января ', ' февраля ', ' марта ',
+        ' апреля ', ' мая ', ' июня ',
+        ' июля ', ' августа ', ' сентября ',
+        ' октября ', ' ноября ', ' декабря '];
+    const date = new Date();
+
+    const addZero = elem => {
+        if (String(elem).length === 1) { 
+            return '0' + elem; 
+        } else {
+        return String(elem);
+        }
+    };
+
+    const changeEnding = (num, timeElem = '') => {
+        const textVariant = (timeElem === 'h' ? [' час ', ' часа ', ' часов '] :
+            timeElem === 'm' ? [' минута ', ' минуты ', ' минут '] :
+            [' секунда ', ' секунды ', ' секунд ']);
+        const n = num % 10;
+
+        return num > 4 && num < 20 ? num + textVariant[2] : 
+            n === 1 ? num + textVariant[0] : 
+            n > 1 && n < 5 ? num + textVariant[1] :
+            num + textVariant[2];
+    };
+
+    const textTime = 'Сегодня ' + week[date.getDay()] + date.getDate() + month[date.getMonth()] + 
+    date.getFullYear() + ' года, ' + changeEnding(date.getHours(), 'h') + 
+    changeEnding(date.getMinutes(), 'm') + changeEnding(date.getSeconds());
+
+    const time = addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' + 
+    addZero(date.getDate()) + '.' + addZero(date.getMonth()) + '.' + date.getFullYear();
+
+    document.querySelector('.text-time').textContent = textTime;
+    document.querySelector('.time').textContent = time;
 };
 
-const setBodyColor = () => {
-    const newColor = getRandomColor();
-    document.body.style.backgroundColor = newColor;
-    document.querySelector('h1').textContent = newColor;
-};
+let elem = document.createElement('div');
+elem.classList.add('text-time');
+document.body.appendChild(elem);
 
-document.querySelector('button').addEventListener('click', setBodyColor);
+elem = document.createElement('div');
+elem.classList.add('time');
+document.body.appendChild(elem);
+
+setInterval(timeDisplay, 1000);
